832af595a096e7b6d639924fe1eeee769cd330fe
jacob-andersen <jacobma87@hotmail.com>
2019-11-20 14:18:09 -0500

    implemented methods for changing text and font size

--- js/FontsProject.js
+++ js/FontsProject.js
@@ -1,3 +1,21 @@
-const fontSize = 40
+const fontSize = "40pt"
 
-console.log('Hello World!')
+function updateCardText() {
+    updateFontSize();
+    var x = document.getElementById("myInput").value;
+    if (x == "") {
+        x = "Then came the night of the first falling star.";
+    }
+
+    for (i = 1; i < 9; i++) {
+        document.getElementById("cardText" + i).innerHTML = x;
+    }
+
+}
+
+function updateFontSize() {
+    for (i = 1; i < 9; i++) {
+        document.getElementById("cardText" + i).style.fontSize = fontSize;
+    }
+
+}
